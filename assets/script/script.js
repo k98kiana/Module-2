1557205be6715b03c877d4daa644560e8910ac96
k98kiana <105411392+k98kiana@users.noreply.github.com>
2022-06-26 20:40:34 -0700

    adding js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,75 @@
-document.getElementsByClassName("box").addEventListener("mouseover", function(){
-    document.getElementsByClassName("box").style.backgroundColor="blue"
+document.getElementById("box1").addEventListener("mouseenter", function(){
+    
+    document.getElementById("box1").style.background="#070769"
+
+})
+
+document.getElementById("box1").addEventListener("mouseout", function(){
+    
+    document.getElementById("box1").style.background="#f5f57f"
+
+})
+
+
+document.getElementById("box2").addEventListener("mouseenter", function(){
+    
+    document.getElementById("box2").style.background="#070769"
+
+})
+
+document.getElementById("box2").addEventListener("mouseout", function(){
+    
+    document.getElementById("box2").style.background="#f5f57f"
+
+})
+
+document.getElementById("box3").addEventListener("mouseenter", function(){
+    
+    document.getElementById("box3").style.background="#070769"
+
+})
+
+document.getElementById("box3").addEventListener("mouseout", function(){
+    
+    document.getElementById("box3").style.background="#f5f57f"
+
+})
+
+
+document.getElementById("box4").addEventListener("mouseenter", function(){
+    
+    document.getElementById("box4").style.background="#070769"
+
+})
+
+document.getElementById("box4").addEventListener("mouseout", function(){
+    
+    document.getElementById("box4").style.background="#f5f57f"
+
+})
+
+
+document.getElementById("box5").addEventListener("mouseenter", function(){
+    
+    document.getElementById("box5").style.background="#070769"
+
+})
+
+document.getElementById("box5").addEventListener("mouseout", function(){
+    
+    document.getElementById("box5").style.background="#f5f57f"
+
+})
+
+
+document.getElementById("box6").addEventListener("mouseenter", function(){
+    
+    document.getElementById("box6").style.background="#070769"
+
+})
+
+document.getElementById("box6").addEventListener("mouseout", function(){
+    
+    document.getElementById("box6").style.background="#f5f57f"
+
 })
