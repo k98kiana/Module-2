6973d834628ccd34e845a82685fd79a6d87e25bd
k98kiana <105411392+k98kiana@users.noreply.github.com>
2022-06-27 18:37:04 -0700

    adding comments to css

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,75 +1,108 @@
 document.getElementById("box1").addEventListener("mouseenter", function(){
     
-    document.getElementById("box1").style.background="#070769"
+    document.getElementById("box1").style.background="#070769";
+    document.getElementById("box1").style.width="500px";
+    document.getElementById("box1").style.height="500px";
 
 })
 
 document.getElementById("box1").addEventListener("mouseout", function(){
     
-    document.getElementById("box1").style.background="#f5f57f"
+    document.getElementById("box1").style.background="#f5f57f";
+    document.getElementById("box1").style.width="300px";
+    document.getElementById("box1").style.height="300px";
 
 })
 
 
 document.getElementById("box2").addEventListener("mouseenter", function(){
     
-    document.getElementById("box2").style.background="#070769"
+    document.getElementById("box2").style.background="#070769";
+    document.getElementById("box2").style.width="500px";
+    document.getElementById("box2").style.height="500px";
+
 
 })
 
 document.getElementById("box2").addEventListener("mouseout", function(){
     
-    document.getElementById("box2").style.background="#f5f57f"
+    document.getElementById("box2").style.background="#f5f57f";
+    document.getElementById("box2").style.width="300px";
+    document.getElementById("box2").style.height="300px";
+
 
 })
 
 document.getElementById("box3").addEventListener("mouseenter", function(){
     
-    document.getElementById("box3").style.background="#070769"
+    document.getElementById("box3").style.background="#070769";
+    document.getElementById("box3").style.width="500px";
+    document.getElementById("box3").style.height="500px";
+
 
 })
 
 document.getElementById("box3").addEventListener("mouseout", function(){
     
-    document.getElementById("box3").style.background="#f5f57f"
+    document.getElementById("box3").style.background="#f5f57f";
+    document.getElementById("box3").style.width="300px";
+    document.getElementById("box3").style.height="300px";
 
 })
 
 
 document.getElementById("box4").addEventListener("mouseenter", function(){
     
-    document.getElementById("box4").style.background="#070769"
+    document.getElementById("box4").style.background="#070769";
+    document.getElementById("box4").style.width="500px";
+    document.getElementById("box4").style.height="500px";
+
 
 })
 
 document.getElementById("box4").addEventListener("mouseout", function(){
     
-    document.getElementById("box4").style.background="#f5f57f"
+    document.getElementById("box4").style.background="#f5f57f";
+    document.getElementById("box4").style.width="300px";
+    document.getElementById("box4").style.height="300px";
+
 
 })
 
 
 document.getElementById("box5").addEventListener("mouseenter", function(){
     
-    document.getElementById("box5").style.background="#070769"
+    document.getElementById("box5").style.background="#070769";
+    document.getElementById("box5").style.width="500px";
+    document.getElementById("box5").style.height="500px";
+
 
 })
 
 document.getElementById("box5").addEventListener("mouseout", function(){
     
-    document.getElementById("box5").style.background="#f5f57f"
+    document.getElementById("box5").style.background="#f5f57f";
+    document.getElementById("box5").style.width="300px";
+    document.getElementById("box5").style.height="300px";
+
 
 })
 
 
 document.getElementById("box6").addEventListener("mouseenter", function(){
     
-    document.getElementById("box6").style.background="#070769"
+    document.getElementById("box6").style.background="#070769";
+    document.getElementById("box6").style.width="500px";
+    document.getElementById("box6").style.height="500px";
+
 
 })
 
 document.getElementById("box6").addEventListener("mouseout", function(){
     
-    document.getElementById("box6").style.background="#f5f57f"
+    document.getElementById("box6").style.background="#f5f57f";
+    document.getElementById("box6").style.width="300px";
+    document.getElementById("box6").style.height="300px";
+
 
 })
