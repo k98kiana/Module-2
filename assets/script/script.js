7c969694dfca50b64eefe134b13b86a2525ea09b
k98kiana <105411392+k98kiana@users.noreply.github.com>
2022-08-02 19:01:03 -0700

    Added link to Cat Webpage I worked on

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,8 @@ document.getElementById("box1").addEventListener("mouseenter", function(){
     document.getElementById("box1").style.background="#070769";
     document.getElementById("box1").style.width="500px";
     document.getElementById("box1").style.height="500px";
+    document.getElementById("catimage").style.width="500px";
+    document.getElementById("catimage").style.height="500px";
 
 })
 
@@ -10,7 +12,10 @@ document.getElementById("box1").addEventListener("mouseout", function(){
     
     document.getElementById("box1").style.background="#f5f57f";
     document.getElementById("box1").style.width="300px";
-    document.getElementById("box1").style.height="300px";
+    document.getElementById("box1").style.height="300px";    
+    document.getElementById("catimage").style.width="300px";
+    document.getElementById("catimage").style.height="300px";
+
 
 })
 
